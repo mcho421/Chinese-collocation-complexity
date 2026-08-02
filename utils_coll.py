2f7bb3729e79e611fb08baaefdaa3648e42daf7d
Mathew Chong <mathewchong.dev@gmail.com>
2026-08-03 08:44:44 +1000

    Refactor: Rename words => dependency_tree

--- utils_coll.py
+++ utils_coll.py
@@ -6,55 +6,55 @@ notHV = ['的', '吗', '吧', '呢', '啊', '呀', '之', '等']
 prepositions = ['把', '被', '对', '给', '跟', '将', '为', '向', '由', '与', '和', '同']
 
 
-def getColl(words):
+def getColl(dependency_tree):
     collocation = []
-    for key, value in words.items():
+    for key, value in dependency_tree.items():
         parent_id = int(value['parent'])
         if parent_id == -1:
             continue
-        parent_cont = words[parent_id]['token']
+        parent_cont = dependency_tree[parent_id]['token']
 
-        if value['pos'] in ['wp', 'ws'] or words[parent_id]['pos'] in ['wp', 'ws']:
+        if value['pos'] in ['wp', 'ws'] or dependency_tree[parent_id]['pos'] in ['wp', 'ws']:
             continue
 
         # P_X_DN
         if value['relation'] == 'POB' and value['pos'] == 'nd':
-            if key > parent_id + 1 and words[parent_id]['pos'] == 'p' and parent_cont in ['在', '到', '从', '自', '自从', '向',
+            if key > parent_id + 1 and dependency_tree[parent_id]['pos'] == 'p' and parent_cont in ['在', '到', '从', '自', '自从', '向',
                                                                                           '往', '除了', '于', '沿着', '至',
                                                                                           '由', '顺着', '朝', '朝着', '沿',
                                                                                           '向着']:
                 coll_p_dn = parent_cont + '\t' + 'X' + '\t' + value['token'] + '\t' + 'P_X_DN'
                 collocation.append(coll_p_dn)
         if value['relation'] == 'POB' and value['token'] in ['时', '时候']:
-            if key > parent_id + 1 and words[parent_id]['pos'] == 'p' and parent_cont in ['在', '当', '每当', '从', '自从',
+            if key > parent_id + 1 and dependency_tree[parent_id]['pos'] == 'p' and parent_cont in ['在', '当', '每当', '从', '自从',
                                                                                           '自', '到']:
                 coll_p_dn = parent_cont + '\t' + 'X' + '\t' + value['token'] + '\t' + 'P_X_DN'
                 collocation.append(coll_p_dn)
 
         # P_X_U
         if value['relation'] == 'RAD' and value['pos'] == 'u' and value['token'] not in notHV:
-            if key > parent_id + 1 and words[parent_id]['pos'] == 'p':
+            if key > parent_id + 1 and dependency_tree[parent_id]['pos'] == 'p':
                 coll_p_u = parent_cont + '\t' + 'X' + '\t' + value['token'] + '\t' + 'P_X_U'
                 collocation.append(coll_p_u)
 
         # CN
         if value['relation'] == 'ATT' and value['pos'] == 'q':
-            if words[parent_id]['pos'] in ['n', 'ni', 'ns', 'nt', 'nz']:
+            if dependency_tree[parent_id]['pos'] in ['n', 'ni', 'ns', 'nt', 'nz']:
                 coll_q_n = value['token'] + '\t' + parent_cont + '\t' + 'Q_N'
                 collocation.append(coll_q_n)
         # AN
         if value['relation'] == 'ATT' and value['pos'] == 'a':
-            if words[parent_id]['pos'] in ['n', 'ni', 'ns', 'nt', 'nz']:
+            if dependency_tree[parent_id]['pos'] in ['n', 'ni', 'ns', 'nt', 'nz']:
                 if key == parent_id - 1:
                     coll_a_n = value['token'] + '\t' + parent_cont + '\t' + 'A_N'
                     collocation.append(coll_a_n)
-                elif key == parent_id - 2 and words[parent_id - 1]['token'] == '的':
+                elif key == parent_id - 2 and dependency_tree[parent_id - 1]['token'] == '的':
                     coll_a_de_n = value['token'] + '\t' + '的' + '\t' + parent_cont + '\t' + 'A_DE_N'
                     collocation.append(coll_a_de_n)
-                elif words[key + 1]['token'] == '的':
+                elif dependency_tree[key + 1]['token'] == '的':
                     coll_a_de_x_n = value['token'] + '\t' + '的' + '\t' + 'X' + '\t' + parent_cont + '\t' + 'A_DE_X_N'
                     collocation.append(coll_a_de_x_n)
-                elif words[parent_id - 1]['token'] == '的':
+                elif dependency_tree[parent_id - 1]['token'] == '的':
                     coll_a_x_de_n = value['token'] + '\t' + 'X' + '\t' + '的' + '\t' + parent_cont + '\t' + 'A_X_DE_N'
                     collocation.append(coll_a_x_de_n)
                 else:
@@ -63,15 +63,15 @@ def getColl(words):
 
         # VO
         if value['relation'] in ['VOB', 'FOB', 'IOB'] and value['pos'] in ['n', 'ni', 'ns', 'nt', 'nz']:
-            if words.__contains__(parent_id + 1) and words[parent_id + 1]['relation'] in ['RAD', 'CMP'] and \
-                    words[parent_id + 1]['token'] not in notHV:
-                if words.__contains__(parent_id + 2) and words[parent_id + 2]['relation'] in ['RAD', 'CMP'] and \
-                        words[parent_id + 2]['token'] not in notHV:
-                    coll_v_2hv_o = parent_cont + '\t' + words[parent_id + 1]['token'] + '\t' + words[parent_id + 2][
+            if dependency_tree.__contains__(parent_id + 1) and dependency_tree[parent_id + 1]['relation'] in ['RAD', 'CMP'] and \
+                    dependency_tree[parent_id + 1]['token'] not in notHV:
+                if dependency_tree.__contains__(parent_id + 2) and dependency_tree[parent_id + 2]['relation'] in ['RAD', 'CMP'] and \
+                        dependency_tree[parent_id + 2]['token'] not in notHV:
+                    coll_v_2hv_o = parent_cont + '\t' + dependency_tree[parent_id + 1]['token'] + '\t' + dependency_tree[parent_id + 2][
                         'token'] + '\t' + value['token'] + '\t' + 'V_2HV_O'
                     collocation.append(coll_v_2hv_o)
                 else:
-                    coll_v_hv_o = parent_cont + '\t' + words[parent_id + 1]['token'] + '\t' + value[
+                    coll_v_hv_o = parent_cont + '\t' + dependency_tree[parent_id + 1]['token'] + '\t' + value[
                         'token'] + '\t' + 'V_HV_O'
                     collocation.append(coll_v_hv_o)
             else:
@@ -80,53 +80,53 @@ def getColl(words):
 
         # SP
         if value['relation'] == 'SBV' and value['pos'] not in ['r', 'nh', 'nl']:
-            if words.__contains__(parent_id + 1) and words[parent_id + 1]['relation'] in ['RAD', 'CMP'] and \
-                    words[parent_id + 1]['token'] not in notHV:
-                if words.__contains__(parent_id + 2) and words[parent_id + 2]['relation'] in ['RAD', 'CMP'] and \
-                        words[parent_id + 2]['token'] not in notHV:
-                    coll_s_v_2hv = value['token'] + '\t' + parent_cont + '\t' + words[parent_id + 1]['token'] + '\t' + \
-                                   words[parent_id + 2]['token'] + '\t' + 'S_V_2HV'
+            if dependency_tree.__contains__(parent_id + 1) and dependency_tree[parent_id + 1]['relation'] in ['RAD', 'CMP'] and \
+                    dependency_tree[parent_id + 1]['token'] not in notHV:
+                if dependency_tree.__contains__(parent_id + 2) and dependency_tree[parent_id + 2]['relation'] in ['RAD', 'CMP'] and \
+                        dependency_tree[parent_id + 2]['token'] not in notHV:
+                    coll_s_v_2hv = value['token'] + '\t' + parent_cont + '\t' + dependency_tree[parent_id + 1]['token'] + '\t' + \
+                                   dependency_tree[parent_id + 2]['token'] + '\t' + 'S_V_2HV'
                     collocation.append(coll_s_v_2hv)
                 else:
-                    coll_s_v_hv = value['token'] + '\t' + parent_cont + '\t' + words[parent_id + 1][
+                    coll_s_v_hv = value['token'] + '\t' + parent_cont + '\t' + dependency_tree[parent_id + 1][
                         'token'] + '\t' + 'S_V_HV'
                     collocation.append(coll_s_v_hv)
-            elif words[parent_id - 1]['token'] not in [':', '：']:
+            elif dependency_tree[parent_id - 1]['token'] not in [':', '：']:
                 coll_s_v = value['token'] + '\t' + parent_cont + '\t' + 'S_V'
                 collocation.append(coll_s_v)
 
         # AP
         if value['relation'] == 'ADV' and value['pos'] in ['a', 'd', 'v']:
             if key == parent_id - 1:
-                if words[parent_id]['pos'] == 'a':
+                if dependency_tree[parent_id]['pos'] == 'a':
                     coll_d_a = value['token'] + '\t' + parent_cont + '\t' + 'D_A'
                     collocation.append(coll_d_a)
-                elif words[parent_id]['pos'] == 'v':
+                elif dependency_tree[parent_id]['pos'] == 'v':
                     coll_d_v = value['token'] + '\t' + parent_cont + '\t' + 'D_V'
                     collocation.append(coll_d_v)
-            elif key < parent_id and words[key + 1]['token'] == '地':
+            elif key < parent_id and dependency_tree[key + 1]['token'] == '地':
                 coll_d_di_v = value['token'] + '\t' + '地' + '\t' + parent_cont + '\t' + 'D_DI_V'
                 collocation.append(coll_d_di_v)
             elif key < parent_id:
-                if words[parent_id]['pos'] == 'a':
+                if dependency_tree[parent_id]['pos'] == 'a':
                     coll_d_x_a = value['token'] + '\t' + 'X' + '\t' + parent_cont + '\t' + 'D_X_A'
                     collocation.append(coll_d_x_a)
-                elif words[parent_id]['pos'] == 'v':
+                elif dependency_tree[parent_id]['pos'] == 'v':
                     coll_d_x_v = value['token'] + '\t' + 'X' + '\t' + parent_cont + '\t' + 'D_X_V'
                     collocation.append(coll_d_x_v)
 
         # PV
         if value['pos'] == 'p' and value['token'] in prepositions:
-            if value['relation'] == 'ADV' and words[parent_id]['pos'] == 'v' and key < parent_id:
-                if words.__contains__(parent_id + 1) and words.__contains__(parent_id + 2) and words[parent_id + 1][
-                    'relation'] in ['RAD', 'CMP'] and words[parent_id + 1]['token'] not in notHV and words[parent_id + 2][
-                    'relation'] in ['RAD', 'CMP'] and words[parent_id + 2]['token'] not in notHV:
-                    coll_p_v_2hv = value['token'] + '\t' + 'X' + '\t' + parent_cont + '\t' + words[parent_id + 1][
-                        'token'] + '\t' + words[parent_id + 2]['token'] + '\t' + 'P_X_V_2HV'
+            if value['relation'] == 'ADV' and dependency_tree[parent_id]['pos'] == 'v' and key < parent_id:
+                if dependency_tree.__contains__(parent_id + 1) and dependency_tree.__contains__(parent_id + 2) and dependency_tree[parent_id + 1][
+                    'relation'] in ['RAD', 'CMP'] and dependency_tree[parent_id + 1]['token'] not in notHV and dependency_tree[parent_id + 2][
+                    'relation'] in ['RAD', 'CMP'] and dependency_tree[parent_id + 2]['token'] not in notHV:
+                    coll_p_v_2hv = value['token'] + '\t' + 'X' + '\t' + parent_cont + '\t' + dependency_tree[parent_id + 1][
+                        'token'] + '\t' + dependency_tree[parent_id + 2]['token'] + '\t' + 'P_X_V_2HV'
                     collocation.append(coll_p_v_2hv)
-                elif words.__contains__(parent_id + 1) and words[parent_id + 1]['relation'] in ['RAD', 'CMP'] and \
-                        words[parent_id + 1]['token'] not in notHV:
-                    coll_p_v_hv = value['token'] + '\t' + 'X' + '\t' + parent_cont + '\t' + words[parent_id + 1][
+                elif dependency_tree.__contains__(parent_id + 1) and dependency_tree[parent_id + 1]['relation'] in ['RAD', 'CMP'] and \
+                        dependency_tree[parent_id + 1]['token'] not in notHV:
+                    coll_p_v_hv = value['token'] + '\t' + 'X' + '\t' + parent_cont + '\t' + dependency_tree[parent_id + 1][
                         'token'] + '\t' + 'P_X_V_HV'
                     collocation.append(coll_p_v_hv)
                 else:
@@ -135,54 +135,54 @@ def getColl(words):
         # PC
         if value['relation'] == 'CMP':
             if key == parent_id + 1:
-                if words.__contains__(key + 1):
-                    if words[key + 1]['token'] in ['了', '得', '过']:
-                        coll_v_c_u = parent_cont + '\t' + value['token'] + '\t' + words[key + 1]['token'] + '\t' + 'V_C_U'
+                if dependency_tree.__contains__(key + 1):
+                    if dependency_tree[key + 1]['token'] in ['了', '得', '过']:
+                        coll_v_c_u = parent_cont + '\t' + value['token'] + '\t' + dependency_tree[key + 1]['token'] + '\t' + 'V_C_U'
                         collocation.append(coll_v_c_u)
                     else:
                         coll_v_c = parent_cont + '\t' + value['token'] + '\t' + 'V_C'
                         collocation.append(coll_v_c)
             elif key == parent_id + 2:
-                if words[key - 1]['token'] in ['了', '得', '过']:
-                    coll_v_u_c = parent_cont + '\t' + words[key - 1]['token'] + '\t' + value['token'] + '\t' + 'V_U_C'
+                if dependency_tree[key - 1]['token'] in ['了', '得', '过']:
+                    coll_v_u_c = parent_cont + '\t' + dependency_tree[key - 1]['token'] + '\t' + value['token'] + '\t' + 'V_U_C'
                     collocation.append(coll_v_u_c)
-                elif words[key - 1]['relation'] == 'ADV':
-                    coll_v_d_c = parent_cont + '\t' + words[key - 1]['token'] + '\t' + value['token'] + '\t' + 'V_D_C'
+                elif dependency_tree[key - 1]['relation'] == 'ADV':
+                    coll_v_d_c = parent_cont + '\t' + dependency_tree[key - 1]['token'] + '\t' + value['token'] + '\t' + 'V_D_C'
                     collocation.append(coll_v_d_c)
-                elif words[key - 1]['relation'] == 'ATT':
-                    if words[key - 1]['pos'] == 'm':
+                elif dependency_tree[key - 1]['relation'] == 'ATT':
+                    if dependency_tree[key - 1]['pos'] == 'm':
                         coll_v_m_c = parent_cont + '\t' + 'm' + '\t' + value['token'] + '\t' + 'V_M_C'
                         collocation.append(coll_v_m_c)
                     else:
                         coll_v_a_c = parent_cont + '\t' + 'A' + '\t' + value['token'] + '\t' + 'V_A_C'
                         collocation.append(coll_v_a_c)
             elif key > parent_id + 2:
-                if words[parent_id + 1]['token'] in ['了', '得', '过']:
+                if dependency_tree[parent_id + 1]['token'] in ['了', '得', '过']:
                     if key == parent_id + 3:
-                        if words[key - 1]['relation'] == 'ATT':
-                            if words[key - 1]['pos'] == 'm':
-                                coll_v_u_m_c = parent_cont + '\t' + words[key - 2]['token'] + '\t' + 'm' + '\t' + value[
+                        if dependency_tree[key - 1]['relation'] == 'ATT':
+                            if dependency_tree[key - 1]['pos'] == 'm':
+                                coll_v_u_m_c = parent_cont + '\t' + dependency_tree[key - 2]['token'] + '\t' + 'm' + '\t' + value[
                                     'token'] + '\t' + 'V_U_M_C'
                                 collocation.append(coll_v_u_m_c)
                             else:
-                                coll_v_u_a_c = parent_cont + '\t' + words[key - 2]['token'] + '\t' + words[key - 1][
+                                coll_v_u_a_c = parent_cont + '\t' + dependency_tree[key - 2]['token'] + '\t' + dependency_tree[key - 1][
                                     'token'] + '\t' + value['token'] + '\t' + 'V_U_A_C'
                                 collocation.append(coll_v_u_a_c)
-                        elif words[key - 1]['relation'] == 'ADV':
+                        elif dependency_tree[key - 1]['relation'] == 'ADV':
                             if value['pos'] != 'v':
-                                coll_v_u_d_c = parent_cont + '\t' + words[key - 2]['token'] + '\t' + words[key - 1][
+                                coll_v_u_d_c = parent_cont + '\t' + dependency_tree[key - 2]['token'] + '\t' + dependency_tree[key - 1][
                                     'token'] + '\t' + value['token'] + '\t' + 'V_U_D_C'
                                 collocation.append(coll_v_u_d_c)
-                            elif words[key + 1]['pos'] == 'wp':
-                                coll_v_u_d_c = parent_cont + '\t' + words[key - 2]['token'] + '\t' + words[key - 1][
+                            elif dependency_tree[key + 1]['pos'] == 'wp':
+                                coll_v_u_d_c = parent_cont + '\t' + dependency_tree[key - 2]['token'] + '\t' + dependency_tree[key - 1][
                                     'token'] + '\t' + value['token'] + '\t' + 'V_U_D_C'
                                 collocation.append(coll_v_u_d_c)
                         else:
-                            coll_v_u_x_c = parent_cont + '\t' + words[key - 2]['token'] + '\t' + 'X' + '\t' + value[
+                            coll_v_u_x_c = parent_cont + '\t' + dependency_tree[key - 2]['token'] + '\t' + 'X' + '\t' + value[
                                 'token'] + '\t' + 'V_U_X_C'
                             collocation.append(coll_v_u_x_c)
                     else:
-                        coll_v_u_x_c = parent_cont + '\t' + words[parent_id + 1]['token'] + '\t' + 'X' + '\t' + value[
+                        coll_v_u_x_c = parent_cont + '\t' + dependency_tree[parent_id + 1]['token'] + '\t' + 'X' + '\t' + value[
                             'token'] + '\t' + 'V_U_X_C'
                         collocation.append(coll_v_u_x_c)
                 else:
